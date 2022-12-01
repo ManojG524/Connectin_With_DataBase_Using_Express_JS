6bc7dd1a8424e02a88ae2614dbac26a6777808cf
ManojG524 <m30843003@gmail.com>
2022-12-01 15:46:06 +0000

    Connecting with th dataBase using the Express JS Code

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+const { open } = require("sqlite");
+const sqlite3 = require("sqlite3");
+const path = require("path");
+
+let dbPath = path.join(__dirname, "goodreads.db");
+
+let db = null;
+
+const initializeDBAndServer = async () => {
+  try {
+    db = await open({
+      filename: dbPath,
+      driver: sqlite3.Database,
+    });
+
+    app.listen(3000, () => {
+      console.log("Server running at http://localhost:3000");
+    });
+  } catch (e) {
+    console.log(`DB ERROR Message ${e.message}`);
+    process.exit(1);
+  }
+};
+
+initializeDBAndServer();
